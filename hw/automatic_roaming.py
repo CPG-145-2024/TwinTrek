7394cf5e92dc078f1e6fe0d7a916018e36f1dbd5
aAryan2404 <jainaaryan2001@gmail.com>
2023-12-18 03:04:56 +0530

    updated automatic_roaming

--- hw/automatic_roaming.py
+++ hw/automatic_roaming.py
@@ -5,27 +5,30 @@ from buggyController  import BuggyController
 
 
 bc = BuggyController()
+bc.setup()
 bc.gpsSetup()
 #set GPIO Pins
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
 
-directions=["Left","Right"]
 
 
-bc.ultrasonicSetup(GPIO_TRIGGER,GPIO_TRIGGER) 
+# bc.ultrasonicSetup()
+
+bc.ultrasonicSetup(GPIO_TRIGGER,GPIO_ECHO) 
 
 def roaming():
-    
+    directions=["Left","Right"]
         
     while True:         
         #keep moving till no obstacle encounter
 
         while(bc.getDistance()>80):
+            print(bc.getDistance())
             bc.forward()
-            time.sleep(4)
+            time.sleep(2)
             direction = random.choice(directions)
-            t = random.random(1,5)
+            t = random.randrange(1,5)
             if(direction=="Left"):
                 bc.left()
                 time.sleep(t)
@@ -39,7 +42,10 @@ def roaming():
         if(bc.getDistance()<=80):
             print("Stop")
             # rotate buggy right while ultrasonic.distance<0.2
-            while(bc.getDistance()<=20):
+            while(bc.getDistance()<=80):
+                print(bc.getDistance())
                 bc.right()
+                time.sleep(1)
             # move buggy in that direction for 5 seconds
+roaming()
             
